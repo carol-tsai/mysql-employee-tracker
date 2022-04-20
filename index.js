137db6454ec35b989df29c5a6b5ab09c2b1fe5e0
Carol <carol.tsai0224@gmail.com>
2022-04-19 21:42:10 -0400

    wrote addEmployee

--- index.js
+++ index.js
@@ -72,30 +72,130 @@ function viewEmployees() {
 roles.salary, CONCAT(m.first_name, ' ' ,  m.last_name) AS manager
 FROM employees e LEFT JOIN employees m ON e.manager_id = m.id
 JOIN roles ON e.role_id = roles.id
-JOIN departments ON roles.department_id = departments.id;`, function(err, result) {
+JOIN departments ON roles.department_id = departments.id ORDER BY id ASC;`, function (err, result) {
       console.table(result);
       mainMenu();
    })
-  
+
 }
 
-// todo
+// Takes in employee name, role, and manager and adds them to the employees table
 function addEmployee() {
+   let roles;
+   let managers;
+   let managersArr
+   db.query('SELECT id, title FROM roles', (err, result) => {
+      roles = result;
+      console.log(roles)
+
+   });
+   db.query("SELECT id, first_name, CONCAT(first_name, ' ' ,last_name) AS name FROM employees",
+      (err, result) => {
+         managers = result
+         managersArr = result.map(manager => { return manager.name });
+         managersArr.unshift('none');
+         console.log("list of managers", managersArr);
+         inquirer.prompt([
+            {
+               type: 'input',
+               name: 'fname',
+               message: "What is the employee's first name?"
+            },
+            {
+               type: 'input',
+               name: 'lname',
+               message: "What is the employee's last name?"
+            },
+            {
+               type: 'list',
+               name: 'role',
+               message: "What is the employee's role?",
+               choices: roles.map(role => { return role.title })
+            },
+            {
+               type: 'list',
+               name: 'manager',
+               message: "Who is the employee's manager?",
+               choices: managersArr
+            },
+         ]).then((data) => {
+            let managerId = null;
+            let roleId;
+            for (let i = 0; i < managers.length; i++) {
+               console.log(managers[i].name, data.manager);
+               if (managers[i].name == data.manager) {
+                  managerId = managers[i].id;
+               }
+            }
+            for (let i = 0; i < roles.length; i++) {
+               if (roles[i].title == data.role) {
+                  roleId = roles[i].id;
+               }
+            }
+            db.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id)
+            VALUES ("${data.fname}", "${data.lname}", ${roleId}, ${managerId})
+            `)
+            console.log(`${data.fname} has been added`)
+            mainMenu();
+         })
+
+      });
 }
 
 // todo
 function updateEmployeeRole() {
+   let employees;
+   let roles;
+   db.query("SELECT CONCAT(first_name, ' ' ,last_name) AS name, role_id FROM employees", (err, result) => {
+      employees = result;
+      console.log(result);
+   });
+   db.query("SELECT id, title FROM roles", (err, result) => {
+      roles = result;
+      console.log("roles")
+      inquirer.prompt([
+         {
+            type: 'list',
+            name: 'name',
+            message: "Which employee's role do you want to update?",
+            choices: employees.map(person => { return person.name })
+         },
+         {
+            type: 'list',
+            name: 'role',
+            message: "Which role do you want to assign to the selected employee?",
+            choices: result.map(role =>{return role.title})
+         }
+      ]).then((data) => {
+         let roleId;
+         let employeeId;
+         for (let i = 0; i < roles.length; i++) {
+            if(roles[i].title == data.role) {
+               roleId = roles[i].id
+            } 
+         }
+         for (let i = 0; i < employees.length; i++) {
+            if(employees[i].name = data.name) {
+               
+            }
+            
+         }
+         db.query(`UPDATE employees SET role = roleId WHERE `)
+         mainMenu()
+      });
+   
+   });
 }
 
 // Prints out all roles with the job title, role id, the department that role belongs to, and the salary for that role
 function viewAllRoles() {
-   db.query(`SELECT title, departments.name AS department, salary
+   db.query(`SELECT id, title, departments.name AS department, salary
 FROM roles
-JOIN departments ON roles.department_id = departments.id;`, function (err, result) {
+JOIN departments ON roles.department_id = departments.id ORDER BY id ASC;`, function (err, result) {
       console.table(result);
       mainMenu();
    });
-   
+
 }
 
 // Takes in role name, salary and department and adds it to the roles table
@@ -103,51 +203,53 @@ async function addRole() {
    let departmentArr = [];
    let departments;
    db.query('SELECT id, name FROM departments'
-   , (err,result) => {
-      departments = result
-      departmentArr = result.map(dep => {
-         return dep.name;
-      });
+      , (err, result) => {
+         departments = result
+         console.log(departments);
+         departmentArr = result.map(dep => {
+            return dep.name;
+         });
 
-      inquirer.prompt([
-         {
-            type: "input",
-            name: "name",
-            message: "What is the name of the role?",
-         },
-         {
-            type: 'input',
-            name: 'salary',
-            message: 'What is the salary of the role?'
-         },
-         {
-            type: 'list',
-            name: 'department',
-            message: 'What department is this role in?',
-            choices: departmentArr,
-         }
-      ]).then((data) => {
-         let deptId;
-         for (let i = 0; i < departments.length; i++) {
-            if(departments[i].name = data.department){
-               deptId = departments[i].id;
-            }            
-         }
-         console.log('department id is ' + deptId);
-         db.query(`INSERT INTO roles(title, department_id, salary) 
-         VALUES ("${data.name}", "${deptId}", "${data.salary}")`);
-         console.log(data.name + " role has been added");
-         mainMenu();
+         inquirer.prompt([
+            {
+               type: "input",
+               name: "name",
+               message: "What is the name of the role?",
+            },
+            {
+               type: 'input',
+               name: 'salary',
+               message: 'What is the salary of the role?'
+            },
+            {
+               type: 'list',
+               name: 'department',
+               message: 'What department is this role in?',
+               choices: departmentArr,
+            }
+         ]).then((data) => {
+            let deptId;
+            for (let i = 0; i < departments.length; i++) {
+               if (departments[i].name == data.department) {
+                  console.log("reached the if statement", departments[i].name, departments[i].id);
+                  deptId = departments[i].id;
+               }
+            }
+            console.log('department id is ' + deptId);
+            db.query(`INSERT INTO roles(title, department_id, salary) 
+         VALUES ("${data.name}", ${deptId}, ${data.salary})`);
+            console.log(data.name + " role has been added");
+            mainMenu();
+         })
       })
-   })
-   
 
-   
+
+
 }
 
 // Prints out departments table
 function viewAllDepartments() {
-   db.query("SELECT * from departments", function (err, result) {
+   db.query("SELECT * from departments ORDER BY id ASC", function (err, result) {
       console.table(result);
       mainMenu();
    })
@@ -160,11 +262,11 @@ function addDepartment() {
       name: "department",
       message: "What is the name of the department?",
    }]).then((data) => {
-      db.query('INSERT INTO departments SET ?', 
-      {name: data.department}, (err) => {
-         console.log("Added " + data.department + " to departments.");
-         mainMenu();
-      })
+      db.query('INSERT INTO departments SET ?',
+         { name: data.department }, (err) => {
+            console.log("Added " + data.department + " to departments.");
+            mainMenu();
+         })
    })
 }
 
